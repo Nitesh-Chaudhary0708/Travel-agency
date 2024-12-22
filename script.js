c27060a5088556fdf8ae306975df4505fa0b490e
Nitesh-Chaudhary0708 <niteshchaudhary5002@gmail.com>
2024-12-22 15:57:30 +0530

    clone

---
+++ script.js
@@ -0,0 +1,14 @@
+// Example Form Validation for Booking Page
+function validateForm() {
+    let name = document.forms["bookingForm"]["name"].value;
+    let email = document.forms["bookingForm"]["email"].value;
+    let date = document.forms["bookingForm"]["date"].value;
+
+    if (name == "" || email == "" || date == "") {
+        alert("Please fill all fields!");
+        return false;
+    } else {
+        alert("Booking Successful!");
+        return true;
+    }
+}
